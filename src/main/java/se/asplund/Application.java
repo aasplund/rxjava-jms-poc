48c2dfc6403a2b8ebf5055f98e671320014c08c6
Anders Asplund (gpg for Anders Asplund) <aasplund@gmail.com>
2015-05-19 22:03:54 +0200

    async with jersey

--- src/main/java/se/asplund/Application.java
+++ src/main/java/se/asplund/Application.java
@@ -1,18 +1,24 @@
 package se.asplund;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 
 @SuppressWarnings("unchecked")
 @ComponentScan
 @EnableAutoConfiguration
 @EnableJms
-public class Application {
+public class Application extends ResourceConfig {
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public Application() {
+		register(AsyncController.class);
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
